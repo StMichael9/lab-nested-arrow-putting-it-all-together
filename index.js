bb4b93d051818b8ce49391e4c9eb862b040544e1
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-13 21:44:44 -0700

    Moved password attempt above attempt count

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const createLoginTracker = (userInfo) => {
     if (locked) {
       return "Account locked due to too many failed login attempts";
     }
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return "Login Failed";
+    }
     attemptCount++;
 
     if (attemptCount >= 3) {
       locked = true;
       return "Account locked due to too many failed login attempts";
-    }
-    if (passwordAttempt === userInfo.password) {
-      return "Login successful";
-    } else {
-      return "Login Failed";
     }
   };
   return innerFunc;
