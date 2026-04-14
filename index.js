47f2f7ed066ca5a2d00932c120415a48c31ca6ae
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-13 21:36:34 -0700

    Updated code

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const createLoginTracker = (userInfo) => {
-  let attemptCount = 0;
   let locked = false;
+
+  let attemptCount = 0;
+
   const innerFunc = (passwordAttempt) => {
+    if (locked) {
+      return "Account locked due to too many failed login attempts";
+    }
     attemptCount++;
 
     if (attemptCount > 3) {
+      locked = true;
       return "Account locked due to too many failed login attempts";
     }
     if (passwordAttempt === userInfo.password) {
