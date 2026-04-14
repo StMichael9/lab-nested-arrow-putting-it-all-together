113e06de8897eb0cf90e4cbc754926869d6a397e
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-13 21:12:54 -0700

    Adding code

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const createLoginTracker = (userInfo) => {
+  let attemptCount = 0;
+  const locked = false;
+  const innerFunc = (passwordAttempt) => {
+    attemptCount++;
+    if (passwordAttempt == userInfo.password) {
+      return "Login successful";
+    } else if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
 
-
+    if (userInfo.password) {
+      return "Loggged In!";
+    } else {
+      return "You are being locked out";
+    }
+  };
+  return innerFunc;
+};
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
